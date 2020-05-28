7fa1e799024e3f3a39e7815d9c34f98e598a219c
Trystan Kaes <60720259+TrystanKaes@users.noreply.github.com>
2020-05-28 13:32:55 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,21 +1,11 @@
 #!/bin/sh
 
 ln -sfv ".dotfiles/runcom/.bash_profile" ~
-ln -sfv ".dotfiles/runcom/.inputrc" ~
 ln -sfv ".dotfiles/git/.gitconfig" ~
-ln -sfv ".dotfiles/runcom/.bashrc" ~
-# ln -sfv ".dotfiles/runcom/.profile" ~
-#ln -sfv ".dotfiles/runcom/.functions" ~
-#ln -sfv ".dotfiles/runcom/.alias" ~
-#ln -sfv ".dotfiles/runcom/.prompt" ~
-#ln -sfv ".dotfiles/git/.gitignore_global" ~
 
-
-
-source ~/.dotfiles/OS_sh/mac.sh
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" #homebrew
 
 tap 'caskroom/cask' #library
 brew 'mas' #appstore
 
-brew cask install 'atom'
-brew cask install 'github'
+source "~/.bash_profile"
