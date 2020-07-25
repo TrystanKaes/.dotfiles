9a428eeeee8a736116e866718ae6281029c85511
Trystan Kaes <trystanblkaes@gmail.com>
2020-07-25 17:32:43 -0600

    update install script

--- install.sh
+++ install.sh
@@ -3,10 +3,15 @@
 chsh -s /bin/bash
 
 ln -sfv ".dotfiles/runcom/.bashrc" ~
+ln -sfv ".dotfiles/runcom/.bash_profile" ~
 ln -sfv ".dotfiles/git/.gitconfig" ~
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)" #homebrew
 
 brew install 'mas' #appstore
+
+echo "Setting case insensitive directory navigation..."
+
+echo 'set completion-ignore-case On' | sudo tee -a /etc/inputrc
 
 source ~/.bashrc
