ba84d2aafd96a0b0a26b8f0456b7905bc89fb604
Trystan Kaes <trystanblkaes@gmail.com>
2021-11-12 13:42:56 -0700

    for loop it

--- setup.sh
+++ setup.sh
@@ -2,16 +2,23 @@
 
 chsh -s /bin/bash
 
-ln -sfv ".dotfiles/runcom/.bashrc" ~
-ln -sfv ".dotfiles/runcom/.bash_profile" ~
-ln -sfv ".dotfiles/runcom/.curlrc" ~
-ln -sfv ".dotfiles/git/.gitconfig" ~
+for filename in \
+    "$HOME"/.dotfiles/runcom/.* \
+    "$HOME"/.dotfiles/profiles/.* \
+    "$HOME"/.dotfiles/git/.* \
+; do
+    if [ -f "$filename" ]; then
+        # shellcheck disable=SC1090
+        cd "$HOME" && ln -sfv "$filename" "$HOME"
+    fi;
+done
 
 echo "Setting case insensitive directory navigation..."
 
 echo 'set completion-ignore-case On' | sudo tee -a /etc/inputrc
 
-."$HOME"/.bashrc
+# shellcheck disable=SC1091
+. "$HOME/.bashrc"
 
 ######## BEGIN MacOS Specific Options ########
 # /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)" #homebrew
